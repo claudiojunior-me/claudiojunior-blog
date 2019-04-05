c67f824be2a1df48ff20bc9526aff33e5b0d84a1
Claudio Junior <claudiojunior@softbox.com.br>
2019-04-05 11:02:11 -0300

    INITIAL COMMIT

--- config.js
+++ config.js
@@ -10,24 +10,24 @@ module.exports = {
   googleAnalyticsId: 'UA-73379983-2',
   menu: [
     {
-      label: 'Articles',
+      label: 'Posts',
       path: '/'
     },
     {
-      label: 'About me',
+      label: 'Sobre Mim',
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
+      label: 'Contato',
       path: '/pages/contacts'
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Claudio Junior',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
-      email: '#',
+      email: 'claudio.1091@gmail.com',
       telegram: '#',
       twitter: '#',
       github: '#',
