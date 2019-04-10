515b43dc7edbe34354dae8abc94b6af3a8621f99
Claudio Junior <claudio.1091@gmail.com>
2019-04-09 21:45:41 -0300

    Fix - personal links

--- config.js
+++ config.js
@@ -28,8 +28,8 @@ module.exports = {
     bio: 'Front End Developer',
     contacts: {
       email: 'claudio.1091@gmail.com',
-      twitter: 'https://twitter.com/ClaudioVsJunior',
-      github: 'https://github.com/claudio1091'
+      twitter: 'ClaudioVsJunior',
+      github: 'claudio1091'
     }
   }
 };
