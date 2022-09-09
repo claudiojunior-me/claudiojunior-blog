2b65bb670bb17a038ee1dcd98af2def4aa44e6e8
Claudio V. Junior <claudiojunior@DESKTOP-08JEFNM>
2022-09-09 18:51:20 -0300

    new version of blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,14 @@
+// const headerNavLinks = [
+//   { href: '/blog', title: 'Blog' },
+//   { href: '/tags', title: 'Tags' },
+//   { href: '/projects', title: 'Projects' },
+//   { href: '/about', title: 'About' },
+// ]
+
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/about', title: 'Sobre' },
+]
+
+export default headerNavLinks
